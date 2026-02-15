89ce3ba6ee8afc9d5ec6a36e4e77cb39818e14db
JesseAlderliesten <jessealderliesten@hotmail.com>
2026-02-15 15:28:48 +0100

    Use function arguments. Do not clean up result.

--- R/get_details_pkgs.R
+++ R/get_details_pkgs.R
@@ -1,46 +1,35 @@
 #' Get details about installed packages
 #'
+#' Wrapper around [utils::installed.packages()] to get slightly more information
+#' about the origin of packages.
+#'
+#' @inheritParams utils::installed.packages
+#'
 #' @returns
-#' A matrix containing the details of the installed packages, returned
-#' [invisibly][invisible()].
+#' A matrix containing the details of the installed packages.
 #'
 #' @family
 #' functions to get information about packages
 #'
 #' @seealso
 #' [tools::CRAN_package_db()] for information about current and archived packages
-#' in the CRAN package repository; [BiocManager::available()] for information
+#' in the CRAN package repository; `BiocManager::available()` for information
 #' about current packages in the Bioconductor and CRAN package repositories.
 #'
 #' The vignette *Instructions about R packages*:
 #' `vignette("r_pkgs", package = "checkrpkgs")`.
 #'
-#' @section Wishlist:
-#' Re-check cleaning `SystemRequirements`: now still yields a separate column.
-#'
-#' See also `progutils_knip::check_status_DEFUNCT()`.
-#'
-#' Implement saving data as a csv-file using `progutils::save_file()`.
+#' @section Programming notes:
+#' `SystemRequirements` is a messy column that makes it difficult to convert the
+#' data to a neat csv-file, see the code of `count_syst_req()` in
+#' `checkrpkgs_knip::count_syst_req.R` for a stub to address that.
 #'
 #' @examples
 #'
 #' @export
-get_details_pkgs <- function() {
+get_details_pkgs <- function(lib.loc = NULL, priority = NULL,
+                             fields = c("Additional_repositories", "Repository",
+                                        "SystemRequirements", "URL")) {
   # Argument 'fields' lists fields that are additional to the default fields.
-  # To do:
-  # - Also get "ReposVer"? Returned by BiocManager::valid()
-  installed_pkgs <- utils::installed.packages(
-    fields = c("SystemRequirements", "Repository"))[
-      , c("Package", "Version", "Built", "NeedsCompilation", "Priority",
-          "LibPath", "SystemRequirements", "Repository")]
-  rownames(installed_pkgs) <- NULL
-
-  # Perform some cleaning of column 'SystemRequirements' to be able to
-  # incorporate it neatly in a csv-file.
-  syst_req <- installed_pkgs[, "SystemRequirements"]
-  syst_req_cleaned <- gsub(pattern = "\\.[[:space:]]+|,[[:space:]]+|;[[:space:]]+",
-                           replacement = "; ", x = syst_req)
-  installed_pkgs[, "SystemRequirements"] <- gsub(
-    pattern = "[[:space:]]+", replacement = " ", x = syst_req_cleaned)
-  installed_pkgs
+  utils::installed.packages(lib.loc = lib.loc, priority = priority, fields = fields)
 }
