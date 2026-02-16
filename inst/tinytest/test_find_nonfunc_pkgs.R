aa3c35a781171891f9c8cbbaa0d1111066adbd44
JesseAlderliesten <jessealderliesten@hotmail.com>
2026-02-16 20:54:00 +0100

    Bugfix to not use recommended packages. Changed return.

--- inst/tinytest/test_find_nonfunc_pkgs.R
+++ inst/tinytest/test_find_nonfunc_pkgs.R
@@ -1,9 +1,7 @@
 # Notes:
-# - Subsequent tests should probably use different packages.
-# - Tests use packages from base-R that are attached during startup if they are
-#   in options("defaultPackages") (see help("Startup")): these packages should
-#   not have an effect; and recommended packages for which it is assumed are not
-#   in options("defaultPackages") such that they are not loaded during startup.
+# - Subsequent tests use different packages from base-R that are known to be
+#   present and functional. These packages are attached during startup if they
+#   are in options("defaultPackages") (see help("Startup")).
 
 
 #### Create objects to use in tests ####
@@ -14,44 +12,47 @@ warn_non_existent_pkgs <- progutils::wrap_text(paste0(
 
 
 #### Test the examples ####
-# This test assumes base package 'base' and recommended package 'grid' are
-# installed and functional.
+# This test assumes base package 'base' are installed and functional.
 expect_message(
   expect_equal(
     find_nonfunc_pkgs(pkgs = c("base", "grid"), quietly = FALSE),
-    character(0)),
+    list(absent = character(0), nonfunc = character(0))
+  ),
   pattern = msg_restart, strict = TRUE, fixed = TRUE)
 
 expect_warning(
   expect_equal(
     find_nonfunc_pkgs(pkgs = non_existent_pkgs, quietly = FALSE),
-    non_existent_pkgs),
+    list(absent = non_existent_pkgs, nonfunc = character(0))
+  ),
   pattern = warn_non_existent_pkgs, strict = TRUE, fixed = TRUE)
 
 expect_warning(
   expect_equal(
     find_nonfunc_pkgs(pkgs = non_existent_pkgs, quietly = TRUE),
-    non_existent_pkgs),
+    list(absent = non_existent_pkgs, nonfunc = character(0))
+  ),
   pattern = warn_non_existent_pkgs, strict = TRUE, fixed = TRUE)
 
 expect_warning(
   expect_equal(
     find_nonfunc_pkgs(pkgs = c(non_existent_pkgs, "utils"), quietly = FALSE),
-    non_existent_pkgs),
+    list(absent = non_existent_pkgs, nonfunc = character(0))
+  ),
   pattern = warn_non_existent_pkgs, strict = TRUE, fixed = TRUE)
 
 
 #### Tests ####
 # Everything before the last slash should be removed. This test assumes base
-# packages 'methods' and 'utils' and recommended packages 'stats4' and 'Matrix'
-# are installed and functional.
-# expect_message(
+# packages 'methods', 'stats', 'stats4' and 'splines' are installed and
+# functional.
+expect_message(
   expect_equal(
-    find_nonfunc_pkgs(pkgs = c("ab/methods", "ab/cd/utils", "ab/stats4",
-                               "ab/cd/Matrix"),
+    find_nonfunc_pkgs(pkgs = c("ab/methods", "ab/cd/stats", "ab/stats4",
+                               "ab/cd/splines"),
                       quietly = TRUE),
-    character(0))
-#,  pattern = msg_restart, strict = TRUE, fixed = TRUE)
+    list(absent = character(0), nonfunc = character(0))),
+  pattern = msg_restart, strict = TRUE, fixed = TRUE)
 
 # Arguments that should result in an error.
 expect_error(
