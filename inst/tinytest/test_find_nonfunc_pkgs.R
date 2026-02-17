fd7f84b2d0763efdd1e1fa73114ceea0e04daa36
JesseAlderliesten <jessealderliesten@hotmail.com>
2026-02-17 17:51:15 +0100

    Remove obsolete message about restarting R.

--- inst/tinytest/test_find_nonfunc_pkgs.R
+++ inst/tinytest/test_find_nonfunc_pkgs.R
@@ -6,19 +6,18 @@
 
 #### Create objects to use in tests ####
 non_existent_pkgs <- c("yz/wx/abcdef4", "wx/abcdef3", "abcdef2", "abcdef1")
-msg_restart <- "Restart R to prevent problems arising from updating loaded packages"
 warn_non_existent_pkgs <- progutils::wrap_text(paste0(
   "non-installed packages: ", progutils::paste_quoted(non_existent_pkgs)))
 
 
 #### Test the examples ####
 # This test assumes base package 'base' are installed and functional.
-expect_message(
+expect_silent(
   expect_equal(
     find_nonfunc_pkgs(pkgs = c("base", "grid"), quietly = FALSE),
     list(absent = character(0), nonfunc = character(0))
-  ),
-  pattern = msg_restart, strict = TRUE, fixed = TRUE)
+  )
+)
 
 expect_warning(
   expect_equal(
@@ -46,13 +45,13 @@ expect_warning(
 # Everything before the last slash should be removed. This test assumes base
 # packages 'methods', 'stats', 'stats4' and 'splines' are installed and
 # functional.
-expect_message(
+expect_silent(
   expect_equal(
     find_nonfunc_pkgs(pkgs = c("ab/methods", "ab/cd/stats", "ab/stats4",
                                "ab/cd/splines"),
                       quietly = TRUE),
-    list(absent = character(0), nonfunc = character(0))),
-  pattern = msg_restart, strict = TRUE, fixed = TRUE)
+    list(absent = character(0), nonfunc = character(0)))
+)
 
 # Arguments that should result in an error.
 expect_error(
